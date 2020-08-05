f5db158207c9f454272847e9de6c27b3e335d728
iampheasant <62655307+iampheasant@users.noreply.github.com>
2020-08-05 21:08:27 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap()
+
+#render_template將會找尋html檔案傳送給使用者
+@app.route("/")
+def page_1():
+    return render_template("page_1.html")
+
+@app.route("/page_2")
+def page_2():
+    return render_template("page_2.html")
+
+@app.route("/page_3")
+def page_3():
+    return render_template("page_3.html")
+
+@app.route("/page_4")
+def page_4():
+    return render_template("page_4.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
