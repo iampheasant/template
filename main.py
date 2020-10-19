8a7a75c89c49269e2e919d423079de9a3d2f7c7c
iampheasant <62655307+iampheasant@users.noreply.github.com>
2020-10-19 12:24:40 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,19 +7,35 @@ bootstrap = Bootstrap()
 #render_template將會找尋html檔案傳送給使用者
 @app.route("/page_1")
 def page_1():
-    return render_template("page_1.html")
+	return render_template("page_1.html")
 
 @app.route("/page_2")
 def page_2():
-    return render_template("page_2.html")
+	return render_template("page_2.html")
 
 @app.route("/page_3")
 def page_3():
-    return render_template("page_3.html")
+	return render_template("page_3.html")
 
 @app.route("/page_4")
 def page_4():
-    return render_template("page_4.html")
+	return render_template("page_4.html")
+
+@app.route("/index")
+def index():
+	return render_template("index.html")
+
+@app.route("/guest")
+def guest():
+	return render_template("guest.html")
+
+@app.route("/reg")
+def reg():
+	return render_template("reg.html")
+
+@app.route("/sign")
+def sign():
+	return render_template("sign.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
